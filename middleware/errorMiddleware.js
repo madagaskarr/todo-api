aeb0df7c22f06800a840f372c2120bade1d7e57f
gevp91 <gevorg.poghosyan@picsart.com>
2023-03-19 14:15:38 +0400

    - Changes response key from data -> details

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -2,6 +2,6 @@ const {sendResponse} = require('../utils/responseHandler');
 
 module.exports = function (err, req, res, next) {
     const { statusCode, message, data } = err;
-    sendResponse(res, statusCode, { message, ...(data && { data }) });
+    sendResponse(res, statusCode, { message, ...(data && { details: data }) });
     next(err)
 };
