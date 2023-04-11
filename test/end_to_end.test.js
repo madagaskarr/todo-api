764729edc56e21f989823e9b07209bcb22d88c51
Tigran Ghazinyan <me@tigranes.io>
2023-04-10 20:39:14 -0700

    added workspace access test

--- test/end_to_end.test.js
+++ test/end_to_end.test.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const request = require("supertest");
 const config = require("config");
 const app = require('../app')
-// const testingDb = require('testing_db')
 
 describe("End to End Integration Tests Suite ", () => {
 
+    // TODO: Instead of using test MongoDB we can SPY on method calls or use the in memory db. Will be implemented later
     beforeEach(async () => {
         await mongoose.connect(config.get('MONGO_URI'));
     });
@@ -105,5 +105,72 @@ describe("End to End Integration Tests Suite ", () => {
         expect(res.statusCode).toBe(200);
         expect(res.body[0].name).toBe("My First Workspace");
         expect(res.body[0].owner).toBe(userId);
+    });
+
+    it("should get unauthorised when trying to access wrong Workspace", async () => {
+
+        // Arrange
+        const tigranRegistrationResponse = await request(app).post("/api/users/register").send({
+            username: "me@tigranes.io",
+            password: "Pass1234!",
+        });
+
+        const tigranAccessToken = tigranRegistrationResponse.body.accessToken;
+        const tigranUserId = tigranRegistrationResponse.body.id;
+
+        const gevorRegistrationResponse = await request(app).post("/api/users/register").send({
+            username: "gev@gmail.com",
+            password: "Pass1234!",
+        });
+
+        const gevorAccessToken = gevorRegistrationResponse.body.accessToken;
+        const gevorUserId = gevorRegistrationResponse.body.id;
+
+        await request(app)
+            .post("/api/workspaces/")
+            .set('Authorization', `Bearer ${gevorAccessToken}`)
+            .send({
+                name: "My First Workspace created By Gevor",
+            });
+
+        await request(app)
+            .post("/api/workspaces/")
+            .set('Authorization', `Bearer ${tigranAccessToken}`)
+            .send({
+                name: "My First Workspace created By Tigran",
+            });
+
+        // Act
+        const gevorWorkspacesResponse = await request(app)
+            .get("/api/workspaces/")
+            .set('Authorization', `Bearer ${gevorAccessToken}`)
+
+        const tigranWorkspacesResponse = await request(app)
+            .get("/api/workspaces/")
+            .set('Authorization', `Bearer ${tigranAccessToken}`)
+
+        const tigranUnauthorisedWorkspacesResponse = await request(app)
+            .get("/api/workspaces/")
+            .set('Authorization', `Bearer WRONG_ACCESS_TOKEN`)
+
+        // Assert
+
+        // Gevor
+        expect(gevorWorkspacesResponse.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(gevorWorkspacesResponse.statusCode).toBe(200);
+        expect(gevorWorkspacesResponse.body.length).toBe(1);
+        expect(gevorWorkspacesResponse.body[0].name).toBe("My First Workspace created By Gevor");
+        expect(gevorWorkspacesResponse.body[0].owner).toBe(gevorUserId);
+
+        // Tigran
+        expect(tigranWorkspacesResponse.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(tigranWorkspacesResponse.statusCode).toBe(200);
+        expect(tigranWorkspacesResponse.body.length).toBe(1);
+        expect(tigranWorkspacesResponse.body[0].name).toBe("My First Workspace created By Tigran");
+        expect(tigranWorkspacesResponse.body[0].owner).toBe(tigranUserId);
+
+        // Tigran Unauthorised
+        expect(tigranUnauthorisedWorkspacesResponse.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(tigranUnauthorisedWorkspacesResponse.statusCode).toBe(401);
     });
 });
